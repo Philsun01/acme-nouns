e128babfb179bfe79714dbd2817fdd9e303656a5
Philsun01 <philsun@gmail.com>
2019-09-16 19:52:33 -0700

    started Person Table

--- db.js
+++ db.js
@@ -1,2 +1,19 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize('postgres://localhost/acme_nouns_db');
+
+const { STRING, UUID, UUIDV4 } = Sequelize;
+
+const Person = conn.define('person',{
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  name: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  }
+});
+//const Place
+//const Thing

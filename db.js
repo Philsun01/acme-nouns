7c3caa508b8762719d4f06c1d5ad22b40eeaca0c
Philsun01 <philsun@gmail.com>
2019-09-16 20:04:34 -0700

    inserted values

--- db.js
+++ db.js
@@ -15,5 +15,44 @@ const Person = conn.define('person',{
     unique: true
   }
 });
-//const Place
-//const Thing
+
+const Place = conn.define('place',{
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  name: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  }
+});
+
+const Thing = conn.define('thing',{
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  name: {
+    type: STRING,
+    allowNull: false,
+    unique: true
+  }
+});
+
+const syncAndSeed = async() => {
+  await conn.sync({force: true});
+  const places = [
+    {name: 'Torrance'},
+    {name: 'Los Angeles'},
+    {name: 'New York'}
+  ];
+
+  const [ Torrance, Los_Angeles, New_York ] = await Promise.all( places.map(place=>Place.create(place) ) )
+
+}
+
+syncAndSeed();
+
